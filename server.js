4203aa10952ac53254965134c4ba5de8d18f7be4
Deja_Gordon <dejagordon80@GMAIL.COM>
2023-02-09 11:26:48 +0100

    DG added seed data

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const bcrypt = require('bcrypt')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 const db = require('./config/db')
+const userSeed = require('./userSeed')
+const User = require('./models/user')
+
 
 // Instantitate DB connection 
 mongoose.connect(db, {useNewUrlParser : true})
@@ -33,7 +36,83 @@ app.use(express.json())
  * Mount imported Routers
 */
 app.use(indexRouter);
-app.use(usersRouter)
+app.use(usersRouter);
+
+
+const dummyUser =
+[
+{
+    firstName: "Nick",
+    lastName: "Langley",
+    userName: "Eddy85",
+    password: "1"
+}, {
+    firstName: "Deja",
+    lastName: "Gorden",
+    userName: "TheCaliforniaCoder",
+    password: "1"
+}, {
+    firstName: "Tom",
+    lastName: "Fairclough",
+    userName: "Tommy",
+    password: "1"
+}, {
+    firstName: "Ma-add",
+    lastName: "Al-Jabouri",
+    userName: "Maad",
+    password: "1"
+}, {
+    firstName: "Ana",
+    lastName: "Solcan",
+    userName: "Ana",
+    password: "1"
+}, {
+    firstName: "Ethan",
+    lastName: "Berk",
+    userName: "EBerk",
+    password: "1"
+}, {
+    firstName: "Hal",
+    lastName: "Evans",
+    userName: "Hally",
+    password: "1"
+}, {
+    firstName: "Justin",
+    lastName: "Tate",
+    userName: "JT",
+    password: "1"
+}, {
+    firstName: "Nora",
+    lastName: "Zics",
+    userName: "Noni",
+    password: "1"
+}, {
+    firstName: "Selina",
+    lastName: "Lavery",
+    userName: "Selina",
+    password: "1"
+}, {
+    firstName: "Usman",
+    lastName: "Bashir",
+    userName: "Usman",
+    password: "1"
+}, {
+    firstName: "Joan",
+    lastName: "Kalanzi",
+    userName: "Joan",
+    password: "1"
+},
+]
+
+
+/* User.insertMany(dummyUser, (error, users) => {
+    if(error) {
+        console.log(error)
+    }else {
+        console.log(users)
+    }
+    
+  }) */
 
 // Ensuring the server is listening to the port
 app.listen(port, () => console.log(`Backend listening on port:${port}`))
