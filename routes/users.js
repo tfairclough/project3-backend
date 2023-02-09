22cc4f845f6185d160ad77edb592c26fbd4c58b9
Deja Gordon <77013067+TheCaliforniaCoder@users.noreply.github.com>
2023-02-09 12:16:10 +0100

    Deja dev (#6)
    * DG - Dejas first commit
    
    * DG added seed data
    
    * DG added get route for users
    
    * DG added put/update for users
    
    * DG deleted duplicate seed data

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 // Require Mongoose Model
 const User = require('./../models/user.js')
+const { unsubscribe } = require('./index.js')
 
 // Instantiate a Router (mini app the only handles routes)
 const router = express.Router()
@@ -24,6 +25,55 @@ router.post('/api/users', (req, res) => {
   // Catch any error that might occur
   .catch((error) => {
     res.status(500).json({ error : error })
+  })
+})
+
+/**
+ * Action:        FIND
+ * Method:        GET
+ * URI:           /api/users
+ * Description:   Create a new Account
+ */
+router.get('/api/users', (req, res) => {
+  User.find()
+  .then((users) => {
+    res.status(201).json({ user: users})
+  })
+  .catch((error) => {
+    res.status(500).json({ error: error})
+  })
+})
+
+
+/**
+ * Action:        DELETE
+ * Method:        DELETE
+ * URI:           /api/users
+ * Description:   Create a new Account
+ */
+router.delete('/api/users/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+  .then((users) => {
+    res.status(201).json({ user: users})
+  })
+  .catch((error) => {
+    res.status(500).json({ error: error})
+  })
+})
+
+/**
+ * Action:        UPDATE
+ * Method:        PUT
+ * URI:           /api/users
+ * Description:   Create a new Account
+ */
+router.put('/api/users/:id', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body.user)
+  .then((users) => {
+    res.status(201).json({ user: users})
+  })
+  .catch((error) => {
+    res.status(500).json({ error: error})
   })
 })
 
