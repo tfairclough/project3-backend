fbef6bf6e2133e2568ba97d028dab184e405d4fd
Nick <nick.langley@hotmail.com>
2023-02-12 17:23:02 +0000

    Made some changes the login api. Did some tidying up of the code by removing unused code and moving the user routes to the user route file and added comments

--- routes/users.js
+++ routes/users.js
@@ -8,25 +8,117 @@ const { unsubscribe } = require('./index.js')
 // Instantiate a Router (mini app the only handles routes)
 const router = express.Router()
 
-/**
+ /**
  * Action:        CREATE
  * Method:        POST
- * URI:           /api/users
- * Description:   Create a new Account
- */
+ * URI:           /api/registerUsers
+ * Description:   Creates a db of users for testing purposes
+ */ 
+// adds dummy users from the frontend and hashes the passwords.
+// to be removed before going live
+app.post('/api/registerUsers', (req, res) => {
+    const users = req.body.user;
+    if (!Array.isArray(users)) {
+      return res.status(400).json({ error: 'User data must be an array' });
+    }
+    console.log(users)
+    let createdUsers = 0;
+    for (const userData of users) {
+        console.log(userData)
+      bcrypt.genSalt(saltRounds)
+        .then((salt) => {
+          console.log(salt);
+          return bcrypt.hash(userData.password, salt);
+        })
+        .then((hash) => {
+          const user = new User({ ...userData, password: hash });
+          return user.save();
+        })
+        .then(() => {
+          createdUsers += 1;
+          if (createdUsers === users.length) {
+            res.status(200).json({ message: 'Users created successfully' });
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: error.message });
+        });
+    }
+  });
 
-router.post('/api/users', (req, res) => {
-  User.create(req.body.user)
-  //On a successful `create action, respond with 201
-  // HTTP status and the content of the new User 
-  .then((newUser) => {
-    res.status(201).json({ users : newUser})
-  })
-  // Catch any error that might occur
-  .catch((error) => {
-    res.status(500).json({ error : error })
-  })
-})
+ /**
+ * Action:        READ
+ * Method:        POST & GET
+ * URI:           /api/login
+ * Description:   Login and return token and current user details
+ */  
+// login authentication and token
+app.post('/api/login', (req, res) => {
+    // Checks if userName and password have values
+    if (req.body.user.userName && req.body.user.password) {
+      // finds one user that matches the userName sent
+      User.findOne({userName: req.body.user.userName})
+        .then((user) => {
+          // if no match the following message is returned
+          if (!user) {
+            return res.status(401).json({ success: false, message: 'user not found' });
+          }
+          
+          // the password being sent is then compared with the password for that user in the db
+          return bcrypt.compare(req.body.user.password, user.password)
+            .then((isMatch) => {
+              if (isMatch) {
+                // adds users id to payload
+                const payload = { id: user.id };
+                // generates token to include payload, secretKey
+                const token = jwt.sign(payload, jwtOptions.secretOrKey);
+                // returns a response that includes the created token and the current user details
+                return res.status(200).json({ token: token, userDetails: user });
+              }
+              // error message if the passwords do not match
+              return res.status(401).json({ success: false, message: 'passwords do not match' });
+            })
+            .catch((error) => {
+              return res.status(500).json({ message: 'error' });
+            });
+        })
+        .catch((error) => {
+          return res.status(500).json({ message: 'error' });
+        });
+    } else {
+      // error message if username and password do not match or not filled out
+      return res.status(400).json({ error: 'Username & Password Required' });
+    }
+  });
+
+ /**
+ * Action:        CREATE
+ * Method:        POST
+ * URI:           /api/register
+ * Description:   Creates a new user, sets a token and returns the users details to be stored localy
+ */        
+// Create single user in register      
+app.post('/api/register', (req, res) => {
+
+    bcrypt.genSalt(saltRounds)
+        .then((salt) => {
+            console.log(salt);
+            return bcrypt.hash(req.body.user.password, salt);
+        })
+        .then((hash) => {
+            const user = new User({ ...req.body.user, password: hash });
+            return user.save();
+        })
+        .then(() => {
+            res.status(200).json({ message: 'User created successfully' });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json({ error: error.message });
+        });
+});
+
 
 /**
  * Action:        FIND
