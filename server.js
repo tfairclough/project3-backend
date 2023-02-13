0908ec2a9abbc3afb80974a5660adfe910093629
3ddy1985 <102239616+3ddy1985@users.noreply.github.com>
2023-02-13 09:14:42 +0000

    Nick login tidy up (#14)
    * NL - Git setup test
    
    * NL - Updated user datatbase to show users instead of user
    
    * changes
    
    * Added authentication to create and login user
    
    * cleaned up some code and added a route to allow multiple users to be added from the frontend
    
    * Made some changes the login api. Did some tidying up of the code by removing unused code and moving the user routes to the user route file and added comments
    
    * Moved the routes back as it broke the process can look at moving them in the week

--- server.js
+++ server.js
@@ -48,130 +48,116 @@ passport.use(strategy);
 app.use(indexRouter);
 app.use(usersRouter);
 
+/**
+ * Action:        CREATE
+ * Method:        POST
+ * URI:           /api/registerUsers
+ * Description:   Creates a db of users for testing purposes
+ */ 
+// adds dummy users from the frontend and hashes the passwords.
+// to be removed before going live
+app.post('/api/registerUsers', (req, res) => {
+  const users = req.body.user;
+  if (!Array.isArray(users)) {
+    return res.status(400).json({ error: 'User data must be an array' });
+  }
+  console.log(users)
+  let createdUsers = 0;
+  for (const userData of users) {
+      console.log(userData)
+    bcrypt.genSalt(saltRounds)
+      .then((salt) => {
+        console.log(salt);
+        return bcrypt.hash(userData.password, salt);
+      })
+      .then((hash) => {
+        const user = new User({ ...userData, password: hash });
+        return user.save();
+      })
+      .then(() => {
+        createdUsers += 1;
+        if (createdUsers === users.length) {
+          res.status(200).json({ message: 'Users created successfully' });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+      });
+  }
+});
+
+/**
+* Action:        READ
+* Method:        POST & GET
+* URI:           /api/login
+* Description:   Login and return token and current user details
+*/  
+// login authentication and token
+app.post('/api/login', (req, res) => {
+  // Checks if userName and password have values
+  if (req.body.user.userName && req.body.user.password) {
+    // finds one user that matches the userName sent
+    User.findOne({userName: req.body.user.userName})
+      .then((user) => {
+        // if no match the following message is returned
+        if (!user) {
+          return res.status(401).json({ success: false, message: 'user not found' });
+        }
         
+        // the password being sent is then compared with the password for that user in the db
+        return bcrypt.compare(req.body.user.password, user.password)
+          .then((isMatch) => {
+            if (isMatch) {
+              // adds users id to payload
+              const payload = { id: user.id };
+              // generates token to include payload, secretKey
+              const token = jwt.sign(payload, jwtOptions.secretOrKey);
+              // returns a response that includes the created token and the current user details
+              return res.status(200).json({ token: token, userDetails: user });
+            }
+            // error message if the passwords do not match
+            return res.status(401).json({ success: false, message: 'passwords do not match' });
+          })
+          .catch((error) => {
+            return res.status(500).json({ message: 'error' });
+          });
+      })
+      .catch((error) => {
+        return res.status(500).json({ message: 'error' });
+      });
+  } else {
+    // error message if username and password do not match or not filled out
+    return res.status(400).json({ error: 'Username & Password Required' });
+  }
+});
+
+/**
+* Action:        CREATE
+* Method:        POST
+* URI:           /api/register
+* Description:   Creates a new user, sets a token and returns the users details to be stored localy
+*/        
 // Create single user in register      
 app.post('/api/register', (req, res) => {
 
-    bcrypt.genSalt(saltRounds)
-        .then((salt) => {
-            console.log(salt);
-            return bcrypt.hash(req.body.user.password, salt);
-        })
-        .then((hash) => {
-            const user = new User({ ...req.body.user, password: hash });
-            return user.save();
-        })
-        .then(() => {
-            res.status(200).json({ message: 'User created successfully' });
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).json({ error: error.message });
-        });
-});
-
-
-// adds dummy users from the frontend and hashes the passwords.
-app.post('/api/registerUsers', (req, res) => {
-    const users = req.body.user;
-    if (!Array.isArray(users)) {
-      return res.status(400).json({ error: 'User data must be an array' });
-    }
-    console.log(users)
-    let createdUsers = 0;
-    for (const userData of users) {
-        console.log(userData)
-      bcrypt.genSalt(saltRounds)
-        .then((salt) => {
+  bcrypt.genSalt(saltRounds)
+      .then((salt) => {
           console.log(salt);
-          return bcrypt.hash(userData.password, salt);
-        })
-        .then((hash) => {
-          const user = new User({ ...userData, password: hash });
+          return bcrypt.hash(req.body.user.password, salt);
+      })
+      .then((hash) => {
+          const user = new User({ ...req.body.user, password: hash });
           return user.save();
-        })
-        .then(() => {
-          createdUsers += 1;
-          if (createdUsers === users.length) {
-            res.status(200).json({ message: 'Users created successfully' });
-          }
-        })
-        .catch((error) => {
+      })
+      .then(() => {
+          res.status(200).json({ message: 'User created successfully' });
+      })
+      .catch((error) => {
           console.error(error);
           res.status(500).json({ error: error.message });
-        });
-    }
-  });
-  
-// login authentication and token
-app.post('/api/login', (req, res) => {
-    if (req.body.user.userName && req.body.user.password) {
-      User.findOne({userName: req.body.user.userName})
-        .then((user) => {
-          if (!user) {
-            return res.status(401).json({ success: false, message: 'user not found' });
-          }
-  
-          return bcrypt.compare(req.body.user.password, user.password)
-            .then((isMatch) => {
-              if (isMatch) {
-                const payload = { id: user.id };
-                const token = jwt.sign(payload, jwtOptions.secretOrKey, { expiresIn: 600 });
-    
-                return res.status(200).json({ success: true, token: token, message: 'User logged in successfully' });
-              }
-  
-              return res.status(401).json({ success: false, message: 'passwords do not match' });
-            })
-            .catch((error) => {
-              return res.status(500).json({ message: 'error' });
-            });
-        })
-        .catch((error) => {
-          return res.status(500).json({ message: 'error' });
-        });
-    } else {
-      return res.status(400).json({ error: 'Username & Password Required' });
-    }
-  });
- 
-   // this is the minimum needed to protect your route from users who aren't logged in
-   app.get('/api/protected', passport.authenticate('jwt', {session: false}), (req, res) => {
-    res.status(200).json({
-      message: 'Hey, you can only see this message with the JSON Web Token.',
-      user: req.user
-    });
-  });
-
-  app.get('/test', (req, res) => {
-    bcrypt.hash('1234', saltRounds, (error, hash) =>{
-        res.status(200).json({ password: hash});
-    });
-}) 
-
-
-
-
-
-app.post('/api/login', (req, res) => {
-    User.findOne(req.body.userName)
-    if (req.body.userName && req.body.password) {
-        if (req.body.userName === User.userName && req.body.password === User.password) {
-            const payload = {
-                userName: User.userName,
-
-            }
-
-            const token = jwt.sign(payload, jwtOptions.secretOrKey, { expiresIn: 600 })
-
-            res.status(200).json({ success: true, token: token })
-        } else {
-            res.status(401).json({ error: 'invalid username or password' })
-        }
-    } else {
-        res.status(400).json({ error: 'Username & Password Required' })
-    }
-})
+      });
+});
 
 
 app.get('/api/protected', passport.authenticate('jwt', {session: false}), (req, res) => {
