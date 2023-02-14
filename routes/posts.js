982b0afef9246db5e1dc5d7ff142794df754e1c2
maljabouri <119860004+maljabouri@users.noreply.github.com>
2023-02-14 15:29:26 +0000

    Ma-Add Post/Feed component added (#16)
    * test commit from personal branch
    
    * bug fix attempt
    
    * added routes for index and users
    
    * added routes
    
    * deleted seed_users.js inside models folder
    
    * added comment regarding populating DB with insert many
    
    * final fix
    
    * fixed comments in users routes
    
    * added post routes and seed data
    
    * added seed data, updated some routing
    
    * fixed routing issues with API regarding post update

---
+++ routes/posts.js
@@ -0,0 +1,66 @@
+// Require necessary NPM Packages
+const express = require('express')
+
+// Require Mongoose Model
+const Post = require('./../models/post.js')
+
+// Instantiate a Router (mini app the only handles routes)
+const router = express.Router()
+
+/**
+ * Action:        CREATE
+ * Method:        POST
+ * URI:           /api/post/create
+ * Description:   Create a new post
+ */
+
+router.post('/api/posts', (req, res) => {
+  Post.create(req.body.user)
+  //On a successful `create action, respond with 201
+  // HTTP status and the content of the new User 
+  .then((newPost) => {
+    res.status(201).json({ users : newPost})
+  })
+  // Catch any error that might occur
+  .catch((error) => {
+    res.status(500).json({ error : error })
+  })
+})
+
+
+/**
+ * Action:        UPDATE
+ * Method:        POST
+ * URI:           /api/users/edit
+ * Description:   edits a post
+ */
+
+router.patch('/api/posts/edit/:id', (req, res) => {
+  Post.findByIdAndUpdate(req.params.id, { content: req.body.content }, { new: true })
+    .then((post) => {
+      res.status(201).json({ post: post });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error });
+    });
+});
+
+
+/**
+ * Action:        FIND
+ * Method:        GET
+ * URI:           /api/posts
+ * Description:   Search an acocunt
+ */
+router.get('/api/posts', (req, res) => {
+  Post.find()
+  .then((posts) => {
+    res.status(201).json({ posts: posts})
+  })
+  .catch((error) => {
+    res.status(500).json({ error: error})
+  })
+})
+
+
+module.exports = router
