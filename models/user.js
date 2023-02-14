fd44b1552fb7ac4fe7fc76910456774648cb9441
3ddy1985 <102239616+3ddy1985@users.noreply.github.com>
2023-02-14 09:55:09 +0000

    Nick register route (#15)
    * added route for registration

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   firstName: { type: String , required: true},
   lastName: { type: String , required: true},
-  userName: { type: String , required: true},
+  userName: { type: String , required: true, unique: true},
   password: { type: String , required: true},
   img: String,
   email: String,
